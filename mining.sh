d3988326c5ac30330c1981d8dfff97563cfc7f01
ammymining <115944657+ammymining@users.noreply.github.com>
2022-10-17 00:14:41 +0700

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RKAzKhSYgGhm9wh2VmdLLMTLYD3dNkSWTT.acer11 -p x --cpu 6
